132460701d19d7452d48e61dc853a77e9b714c1e
xia0t99 <xia0t99@gmail.com>
2015-04-25 15:32:03 +0800

    Quiz Checking Improve

--- user/lessons_info.php
+++ user/lessons_info.php
@@ -225,10 +225,8 @@ $(document).ready(function(){
                       else
                       {
                        window.location.href ="userHome.php"
-                /    } 
+                    } 
                 </script>';
-                echo '<script language="JavaScript"> window.location.href ="questions.php?qid='. $done_lessonid . '" </script>';
-
                 
             }
 
