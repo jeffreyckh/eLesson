70fd3e23ec64aae7ccf61fd96be6e7349baf54f1
jeffreyckh <jeffrey_ckh@hotmail.com>
2015-05-11 02:32:52 +0800

    userhome page

--- user/userHome.php
+++ user/userHome.php
@@ -28,39 +28,108 @@ $announcement = new announcementView();
   <title>Home</title>
   <link rel="stylesheet" href="home.css" type="text/css" media="screen" />
   <link rel="stylesheet" href="../jscss/default.css" type="text/css" media="screen" />
-    <link rel="stylesheet" type="text/css" href="../jscss/dist/css/bootstrap.min.css"> 
+   <link rel="stylesheet" href="../jscss/tablesorter/css/theme.blue.css">
+    <link rel="stylesheet" type="text/css" href="../jscss/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="../jscss/datatable/jquery.dataTables.min.css">
     <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
     <script src="../jscss/jquery.js"></script>
     <!-- Include all compiled plugins (below), or include individual files as needed -->
     <script src="../jscss/dist/js/bootstrap.min.js"></script>
-    <script src="../jscss/ckeditor/ckeditor.js"></script>
+    <script src="../jscss/datatable/jquery.dataTables.min.js"></script> 
+     <script src="../jscss/datatable/jquery.dataTables.bootstrap.js"></script>
 </head>
 <body>
-  <div class = "col-md-7">
-    <legend>Navigation</legend>
+<div class = "col-md-6">
     <div class = "row">
-      <div class = "col-md-2">
-        <figure>
-        <a href="courses.php"><img src="../img/blackboard.png"></a>
-        <figcaption>Course</figcaption>
-        </figure>
-      </div>
-      <div class = "col-md-2">
-        <figure>
-        <a href="announcement.php"><img src="../img/announcement.png"></a>
-        <figcaption>Announcement</figcaption>
-        </figure>
-      </div>
+      
+        <table id = "incomplete" class="table table-striped table-bordered" cellspacing="0">
+          <p><b>Incomplete Lesson</b></p>
+        <thead>
+            <th align="right">Course</th>
+            <th align="right">Lesson</th>
+        </thead>
+        <?php
+          $incompquery = "SELECT * FROM lessoncomplete WHERE userid = '$uid'AND complete = '0'";
+          $incompresult = mysql_query($incompquery);
+          echo "<tbody>";
+          while($incomp_rows = mysql_fetch_object($incompresult))
+          {
+            $lessonid = $incomp_rows->lessonid;
+            $inlesson = "SELECT *FROM lesson where lessonid = '$lessonid'";
+            $lessonresult = mysql_query($inlesson) or die(mysql_error());
+            while ($l_rows=mysql_fetch_object($lessonresult))
+            {
+              $lessonname = $l_rows->lessonname;
+              $courseid = $l_rows->direction_id;
+              $incourse = "SELECT * FROM course WHERE courseid = '$courseid'";
+              $courseresult = mysql_query($incourse) or die(mysql_error());
+              while ($c_rows = mysql_fetch_object($courseresult)) 
+              {
+                $coursename = $c_rows->coursename;
+                ?>
+                 <tr>
+                    <td align="left"><?php echo $coursename ?></a></td>
+                    <td align="left"><?php echo "
+                       <a href=\"lessons_info.php?lid=$lessonid\">".$lessonname."</a>"; ?></a></td>
+                </tr>
+          <?php
+              }
+            }
+          }
+        ?>
+      </tbody>
+    </table>
     </div>
+<!-- complete lesson !-->
+  <div class = "row">
+      <table id = "Complete" class="table table-striped table-bordered" cellspacing="0">
+        <p><b>Complete Lesson</b></p>
+        <thead>
+            <th align="right">Course</th>
+            <th align="right">Lesson</th>
+        </thead>
+        <?php
+          $compquery = "SELECT * FROM lessoncomplete WHERE userid = '$uid'AND complete = '1'";
+          $compresult = mysql_query($compquery);
+          echo "<tbody>";
+          while($comp_rows = mysql_fetch_object($compresult))
+          {
+            $lessonid = $comp_rows->lessonid;
+            $complesson = "SELECT *FROM lesson where lessonid = '$lessonid'";
+            $complessonresult = mysql_query($complesson) or die(mysql_error());
+            while ($compl_rows=mysql_fetch_object($complessonresult))
+            {
+              $complessonname = $compl_rows->lessonname;
+              $compcourseid = $compl_rows->direction_id;
+              $compcourse = "SELECT * FROM course WHERE courseid = '$courseid'";
+              $compcourseresult = mysql_query($compcourse) or die(mysql_error());
+              while ($compc_rows = mysql_fetch_object($compcourseresult)) 
+              {
+                $compcoursename = $compc_rows->coursename;
+                ?>
+                 <tr>
+                    <td align="left" ><?php echo $compcoursename ?></a></td>
+                    <td align="left" ><?php echo "
+                       <a href=\"lessons_info.php?lid=$lessonid\">".$complessonname."</a>"; ?></a></td>
+                </tr>
+          <?php
+              }
+            }
+          }
+        ?>
+      </tbody>
+    </table>
   </div>
-  <div class = "col-md-3">
+</div>
+  <div class = "col-md-2">
+  </div>
+  <div class = "col-md-2">
   <div class = "row">
   <?php
   include "../inc/calender.php";
   ?>
   <br></br>
     <div class = "row">
-    <div class="panel-group" id="accordion" role="tablist" aria-multiselectable="true">
       <div class="panel panel-default">
         <div class="panel-heading" role="tab" id="headingOne">
           <h4 class="panel-title">
@@ -69,7 +138,7 @@ $announcement = new announcementView();
             </a>
           </h4>
         </div>
-      <div id="collapseOne" class="panel-collapse collapse in" role="tabpanel" aria-labelledby="headingOne">
+      <div id="collapseOne" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingOne">
         <div class="panel-body">
             <fieldset class = "setright">
               <?php
@@ -82,7 +151,6 @@ $announcement = new announcementView();
                      <br></br>
                      Annoucement:".$a_rows->taskname."
                      </fieldset>";
-                    echo "<hr>";
                   }
               ?>      
             </fieldset>
@@ -91,62 +159,10 @@ $announcement = new announcementView();
       </div>
     </div>
   </div>
-  <div class = "row">
-    <div class="panel-group" id="accordion" role="tablist" aria-multiselectable="true">
-      <div class="panel panel-default">
-        <div class="panel-heading" role="tab" id="headingOne">
-          <h4 class="panel-title">
-            <a data-toggle="collapse" data-parent="#accordion" href="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-            Lastest View
-            </a>
-          </h4>
-        </div>
-      <div id="collapseOne" class="panel-collapse collapse in" role="tabpanel" aria-labelledby="headingOne">
-        <div class="panel-body">
-            <fieldset class = "setright">
-              <?php
-                  $username = $_SESSION['username'];
-                  //echo $username;
-                  $query1 = "SELECT * FROM user WHERE username= '$username' limit 1";
-                  $result1=mysql_query($query1);
-                  
-              
-                  while($rows=mysql_fetch_object($result1))
-                  {
-
-                    $_SESSION['userid']=$rows->userid;
-                    $uid = $_SESSION['userid'];
-                  }
-
-                  $query2="select * from user_to_lesson WHERE userid = '$uid' ";
-                  $result2=mysql_query($query2);
-                  while($l_rows=mysql_fetch_object($result2))
-                  {
-                    $lessonid = $l_rows->lessonid;
-                    $query3="select * from lesson WHERE lessonid = '$lessonid' ";
-                    $result3=mysql_query($query3);
-                    while($d_rows=mysql_fetch_object($result3))
-                    {
-                      $lessonname = $d_rows->lessonname;
-                      echo "<fieldset>
-                       
-                       <a href=\"lessons_info.php?lid=$lessonid\">".$lessonname."</a>
-                      </fieldset>";
-                      echo "<hr>";
-                    }
-                  }
-
-                  
-              ?>      
-            </fieldset>
-        </div>
-      </div>
-      </div>
-    </div>
-  </div>
-</div>
 </div>
 
-
+<script type="text/javascript">
+        $(".collapse").collapse();
+</script>
 </body>
 </html>
